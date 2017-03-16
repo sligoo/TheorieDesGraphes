84f3383f40926d6b0a9f6e850cd1da2a1df7377d
sligoo <sacha.liguori@icloud.com>
2017-03-16 09:20:18 +0100

    Debut projetML

--- Projet/projet.ml
+++ Projet/projet.ml
@@ -1,9 +1,25 @@
+open Dag;;
 (*******************************************)
 (*  Projet théorie des graphes 2016/2017   *)
 (* Par Nicolas Surbayrole et Sacha Liguori *)
 (*******************************************)
 
+<<<<<<< Updated upstream
 open Dag;;
+=======
+
+(* predInZ *)
+(* Ajouter a la liste y,les noeuds se trouvant dans la liste *)
+(* et dont les predecesseurs sont dans Z *)
+(* Parametres: 
+	-graphe: graphe étudié 
+	-noeuds: liste des noeuds 
+	-y: la liste dans laquelle ajouter les noeuds 
+	-z: la liste dans laquelle les successeurs doivent etre
+*)
+let rec predInZ graphe noeuds y z =
+
+>>>>>>> Stashed changes
 
 (* Question 6 *)
 (* Fonction: Tri Topologique *)
@@ -15,6 +31,7 @@ open Dag;;
    - vous implementerez l'algorithme de tri topologique de l'enonce, en utilisant un format de file pour Y (section 1)
    *)
 
+<<<<<<< Updated upstream
 (* renvoie la liste des sommets sans prédécesseur *)
 let source t = (Dag.fold_vertex (fun v l -> if ((Dag.pred t v) = []) then v::l else l) t []);;
 
@@ -27,3 +44,6 @@ let rec print_liste_vectex l =
 
 (*let tri_topologique t = *)
 	
+=======
+	
+>>>>>>> Stashed changes
