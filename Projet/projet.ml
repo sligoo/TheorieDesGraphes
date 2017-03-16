28a0f168faa572e62a74b98094878fa061c4a613
Nicolas SURBAYROLE <nicolas.surbayrole@inpt.fr>
2017-03-16 09:16:05 +0100

    Première fonction

--- Projet/projet.ml
+++ Projet/projet.ml
@@ -3,6 +3,8 @@
 (* Par Nicolas Surbayrole et Sacha Liguori *)
 (*******************************************)
 
+open Dag;;
+
 (* Question 6 *)
 (* Fonction: Tri Topologique *)
 (* entrees: 
@@ -13,5 +15,15 @@
    - vous implementerez l'algorithme de tri topologique de l'enonce, en utilisant un format de file pour Y (section 1)
    *)
 
-let tri_topologique t =
+(* renvoie la liste des sommets sans prédécesseur *)
+let source t = (Dag.fold_vertex (fun v l -> if ((Dag.pred t v) = []) then v::l else l) t []);;
+
+(* print les sommets contenus dans une liste *)
+let rec print_liste_vectex l = 
+    match l with
+      | [] -> ()
+      | h::t -> ( (print_endline (Dag.namev h)); (print_liste_vectex t));;
+
+
+(*let tri_topologique t = *)
 	
