29e75c3ab8f42afe9f7675310266a5b0c4acb5f6
sligoo <sacha.liguori@icloud.com>
2017-03-16 08:11:17 +0100

    ajout projet.ml

---
+++ Projet/projet.ml
@@ -0,0 +1,16 @@
+(*******************************************) 
+(*  Projet théorie des graphes 2016/2017   *) 
+(* Par Nicolas Surbayrole et Sacha Liguori *) 
+(*******************************************) 
+ 
+(* Question 6 *) 
+(* Fonction: Tri Topologique *) 
+(* entrees:  
+   - un DAG 
+   sorties: 
+   - une liste des sommets du DAG ordonnee selon un tri topologique  
+   specifs:  
+   - vous implementerez l'algorithme de tri topologique de l'enonce, en utilisant un format de file pour Y (section 1) 
+   *) 
+ 
+let tri_topologique t = 
